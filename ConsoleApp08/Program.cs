651a737466d4f4e9d81f65ac4179db2c5a6e9160
mana.tokorocchi8@gmail.com <mana.tokorocchi8@gmail.com>
2026-03-09 17:49:29 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Console.Write("コンソールに文字列１を入力：");
+            string message1 = Console.ReadLine();
+            Console.Write("コンソールに文字列２を入力：");
+            string message2 = Console.ReadLine();
+            Console.WriteLine( message1 + message2 );
+        }
+    }
+}
